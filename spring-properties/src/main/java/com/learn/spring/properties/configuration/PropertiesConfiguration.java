5ea074d5ffe775ed42915e6d6677536eb3609e03
庞云龙 <yunlongpang@sohu-inc.com>
2018-02-28 15:43:10 +0800

    add configuration field setter

--- spring-properties/src/main/java/com/learn/spring/properties/configuration/PropertiesConfiguration.java
+++ spring-properties/src/main/java/com/learn/spring/properties/configuration/PropertiesConfiguration.java
@@ -3,7 +3,6 @@ package com.learn.spring.properties.configuration;
 import com.learn.spring.properties.entity.UserInfo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
@@ -13,23 +12,14 @@ import org.springframework.core.io.Resource;
  * @author yunlongpang 2018/2/28
  */
 @Configuration
-@ComponentScan(basePackages = "com.learn.spring.properties")
 public class PropertiesConfiguration {
 
 
-    @Value("${user.name}")
+    @Value("${user.info.name}")
     private String name;
 
     @Value("${user.age}")
     private int age;
-
-    @Bean
-    public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
-        Resource resource = new ClassPathResource("config.properties");
-        configurer.setLocation(resource);
-        return configurer;
-    }
 
     @Bean
     public UserInfo userInfo() {
@@ -37,6 +27,18 @@ public class PropertiesConfiguration {
         userInfo.setName(name);
         userInfo.setAge(age);
         return userInfo;
+    }
+
+    /**
+     * PropertySourcesPlaceholderConfigurer必须使用static才能使用@Value注入属性值
+     * @return PropertySourcesPlaceholderConfigurer
+     */
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        Resource resource = new ClassPathResource("user.properties");
+        configurer.setLocation(resource);
+        return configurer;
     }
 
 }
