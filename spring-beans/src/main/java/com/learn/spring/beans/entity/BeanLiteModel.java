fc410ed0a85edee96b906158f00a895b9dc954fe
庞云龙 <yunlongpang@sohu-inc.com>
2018-03-02 10:06:46 +0800

    fix bug

--- spring-beans/src/main/java/com/learn/spring/beans/entity/BeanLiteModel.java
+++ spring-beans/src/main/java/com/learn/spring/beans/entity/BeanLiteModel.java
@@ -5,6 +5,12 @@ import org.springframework.stereotype.Component;
 
 /**
  * @author yunlongpang 2018/3/2
+ * Bean methods in lite mode will be treated as plain factory methods by the container
+ * (similar to factory-method declarations in XML), with scoping and lifecycle callbacks properly applied.
+ * The containing class remains unmodified in this case, and there are no unusual constraints for the containing class or the factory methods.
+ * In contrast to the semantics for bean methods in @Configuration classes, 'inter-bean references' are not supported
+ * in lite mode. Instead, when one @Bean-method invokes another @Bean-method in lite mode,
+ * the invocation is a standard Java method invocation; Spring does not intercept the invocation via a CGLIB proxy.
  */
 @Component
 public class BeanLiteModel {
--- spring-beans/src/test/java/com/learn/spring/beans/junit/SpringBeanJunit.java
+++ spring-beans/src/test/java/com/learn/spring/beans/junit/SpringBeanJunit.java
@@ -1,11 +1,11 @@
 package com.learn.spring.beans.junit;
 
+import com.learn.spring.beans.configuration.SpringBeanConfiguration;
 import com.learn.spring.beans.entity.LessonInfo;
 import com.learn.spring.beans.entity.UserInfo;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ScannedGenericBeanDefinition;
 
 /**
  * @author yunlongpang 2018/3/2
@@ -14,7 +14,7 @@ public class SpringBeanJunit {
 
     @Test
     public void litModelBeanJunit() {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ScannedGenericBeanDefinition.class);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringBeanConfiguration.class);
         UserInfo userInfo = applicationContext.getBean(UserInfo.class);
         System.out.println(userInfo);
         LessonInfo lessonInfo = applicationContext.getBean(LessonInfo.class);
